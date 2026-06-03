2dcdfe497af455473bfda03db9114083481ce0d1
seha090425htsrrna1-ux <seha090425htsrrna1@cbtis125.edu.mx>
2026-06-03 12:29:06 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    
+    $localhost = "localhost";
+    $username = "root";
+    $password = "";
+    $base_de_datos = "mininet";
+
+    $conexion = mysqli_connect($localhost, $username, $password, $base_de_datos);
+
+    ?>
